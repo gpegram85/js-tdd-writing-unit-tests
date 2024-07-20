d4c3a9b3d3151bffdfaf809f584dfce021c7f86b
Grey Pegram <dpegram85@gmail.com>
2024-07-20 19:25:10 -0400

    Completed Lab

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,8 @@
 // Your code here
 export function pointsForWord(word) {
-    let points = 0
+    let points = 0;
     for (const char of word) {
-        if(["a", "e", "i", "o", "u"].includes(char)) {
-            points += 1
-        } else {
-            points += 2
-        }
+      points += /[aeiou]/i.test(char) ? 1 : 2;
     }
-    return points
-}
+    return points;
+  }
